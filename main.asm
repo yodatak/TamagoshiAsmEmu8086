7c41336de0684aebd97957667faf23b974840995
germinolegrand <germinolegrand@gmail.com>
2013-05-26 04:52:30 +0200

    ajout de documentation pour show_sprite

--- main.asm
+++ main.asm
@@ -26,26 +26,26 @@ ret
 
 ;#afficher un pixel#
 ;
-;cx  colonne
-;dx  ligne
+;cx colonne
+;dx ligne
 ;               
-;al  couleur
-;0   black
-;1   blue
-;2   green
-;3   cyan
-;4   red
-;5   magenta
-;6   brown
-;7   light gray
-;8   dark gray
-;9   light blue
-;A   light green
-;B   light cyan
-;C   light red
-;D   light magenta
-;E   yellow      
-;F   white
+;al couleur
+;   0 black
+;   1 blue
+;   2 green
+;   3 cyan
+;   4 red
+;   5 magenta
+;   6 brown
+;   7 light gray
+;   8 dark gray
+;   9 light blue
+;   A light green
+;   B light cyan
+;   C light red
+;   D light magenta
+;   E yellow      
+;   F white
 ;
 ;mov ah, 0ch
 ;int 10h                      
@@ -54,46 +54,51 @@ sprite_pikamini_w DW 8
 sprite_pikamini_h DW 3
 sprite_pikamini   DB 04h,04h,04h,04h,04h,04h,04h,04h, 04h,0Fh,0Fh,0Fh,0Fh,0Fh,0Fh,04h, 04h,0Fh,0Fh,0Fh,0Fh,0Fh,0Fh,04h
 
-show_sprite_spriteptr DW ?
+;#afficher un sprite#
+;
+;cx position x
+;dx position y
+;si adresse des pixels
+;
+;call show_sprite
 
 show_sprite PROC
+    push cx ;sprite_x
+    push dx ;sprite_y
+    mov di, sp
     
-    mov bx, si
-    sub bx, 04h
-    mov show_sprite_spriteptr, bx
-    
-      
-    mov ax, [si-4]
-    mul [si-2]
+    mov ax, w.[si-4] ;sprite_w
+    mul w.[si-2] ;sprite_h
     mov bx, ax
+                      
+    mov dx, [di+2] ;sprite_y
+    add dx, w.[si-2] ;sprite_h
     
-    push cx
-    push dx              
-    add dx, [si-2]
-    
-    show_sprite_loop1:     
-        dec dx
+    rows:     
+        dec dx        
         
-        pop cx ;sprite_x
-        push cx     
-        add cx, sprite_w
-        show_sprite_loop2:
+        mov cx, w.[di+4] ;sprite_x     
+        add cx, w.[si-4] ;sprite_w
+        columns:
             dec cx   
             dec bx
             
-            mov al, show_sprite_spriteptr[bx]  
+            mov al, b.[si+bx] ;pixel_color
             mov ah, 0ch ;put pixel
             int 10h  
             
-            
-            cmp cx, sprite_x
-            ja show_sprite_loop2
+            cmp cx, w.[di+4] ;sprite_x
+            ja columns
+     
+        cmp dx, w.[di+2] ;sprite_y
+        ja rows 
         
-        pop ax ;sprite_y  
-        cmp dx, ax
-        ja show_sprite_loop1
+    pop dx
+    pop cx
     
     ret
-show_sprite ENDP             
+show_sprite ENDP
+
+
 
 END
