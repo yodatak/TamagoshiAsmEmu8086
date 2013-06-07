50b1f61bcecb3a5c9f733c51d2bb8174db1aeb3a
germinolegrand <germinolegrand@gmail.com>
2013-06-08 01:19:57 +0200

    oubli précédent
    mineur.

--- tools/gen.bat
+++ tools/gen.bat
@@ -1,6 +1,6 @@
 ::les sprites des différentes évolutions
-SpriteMaker.exe "../images/pikamini.png" "sprite_pikamini" > pikamini.txt
 ::SpriteMaker.exe "../images/pikaegg.jpg" "sprite_pikaegg" > pikaegg.txt
+SpriteMaker.exe "../images/pikamini.png" "sprite_pikamini" > pikamini.txt
 ::SpriteMaker.exe "../images/pika.jpg" "sprite_pika" > pika.txt
 ::SpriteMaker.exe "../images/rai.jpg" "sprite_rai" > rai.txt
 
